5f945a9911942b0ea96489e0e12ad5528bcb5501
Taichi Yamada <ichirakumen@gmail.com>
2024-01-31 00:38:01 +0800

    Finished Section 6, 7, and 8

--- app.js
+++ app.js
@@ -13,3 +13,58 @@ function show() {
 function close() {
     mainMenu.style.top = '-100%'
 }
+
+function initMap() {
+    var location  = {lat: 14.119420, lng: 122.949898};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
+const draggableElement = document.querySelector("#draggable-element");
+
+draggableElement.addEventListener("dragstart", e => {
+    e.dataTransfer.setData("text/plain", draggableElement.id);
+});
+
+for (const dropZone of document.querySelectorAll(".drop-zone")) {
+    dropZone.addEventListener("dragover", e => {
+        e.preventDefault();
+        dropZone.classList.add("drop-zone--over");
+    });
+
+    dropZone.addEventListener("dragleave", e => {
+        dropZone.classList.remove("drop-zone--over");
+    });
+
+    dropZone.addEventListener("drop", e => {
+        e.preventDefault();
+
+        const droppedElementId = e.dataTransfer.getData("text/plain");
+        const droppedElement = document.getElementById(droppedElementId);
+
+        dropZone.appendChild(droppedElement);
+        dropZone.classList.remove("drop-zone--over");
+    });
+}
+
+const input  = document.getElementById("type-content"),
+h2 = document.getElementById("change-content");
+
+h2.innerHTML = localStorage.getItem("value");
+
+function display() {
+    localStorage.setItem("value", input.value);
+    h2.innerHTML = localStorage.getItem("value");
+}
+
+function reset() {
+    input.value = '';
+    localStorage.clear();
+    h2.innerHTML = '';
+}
